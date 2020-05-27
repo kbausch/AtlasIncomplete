6e153efaeb91106ec29073ccb04f97d2261328be
Kaleb Bausch <kbausch@mtech.edu>
2020-05-26 22:15:33 -0600

    Got nav page set up and fixed some little things

--- AtlasIncomplete/src/app/app-routing.module.ts
+++ AtlasIncomplete/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './maincontentdisplay/landing-page/landing-page.component';
+import { MainnavPageComponent } from './maincontentdisplay/mainnav-page/mainnav-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : LandingPageComponent},
+  {path: 'mainnav-page', component : MainnavPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AtlasIncomplete/src/app/maincontentdisplay/mainnav-page/mainnav-page.component.ts
+++ AtlasIncomplete/src/app/maincontentdisplay/mainnav-page/mainnav-page.component.ts
@@ -12,4 +12,13 @@ export class MainnavPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public navList: Object[]= [
+    {title: 'Characters', routerlink: '/mainnav-page', image:'https://66.media.tumblr.com/4aadb561ee128380f81a50f857e5f38b/tumblr_phdlg4LwT81u4vhy0o1_r1_1280.png'},
+    {title: 'Lore and Mechanics', routerlink: '/mainnav-page'},
+    {title: 'About', routerlink: '/mainnav-page'},
+    {title: 'Watch', routerlink: '/mainnav-page'},
+    {title: 'Sick Loot', routerlink: '/mainnav-page'},
+    {title: 'Forums', routerlink: '/mainnav-page'}
+  ];
+
 }
