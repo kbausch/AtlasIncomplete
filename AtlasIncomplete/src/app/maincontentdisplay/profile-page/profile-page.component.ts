ef1eeb02d1f1a55204f845c5bc19971e3f815749
Kaleb Bausch <kbausch@mtech.edu>
2020-05-29 22:08:04 -0600

    small quality of life updates for this BAD setup

--- AtlasIncomplete/src/app/maincontentdisplay/profile-page/profile-page.component.ts
+++ AtlasIncomplete/src/app/maincontentdisplay/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  signOut(){
+    sessionStorage.setItem('verified', 'false');
+    this.route.navigateByUrl('/');
+  }
 }
